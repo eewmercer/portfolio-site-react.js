c0e929fb1545d3e16e4a4d2d7c2a8d8d8c10de18
Emmaline Mercer <eewmercer@gmail.com>
2024-09-15 23:17:02 -0400

    Create Home component for the navigation menu

---
+++ src/Components/HomeHead.tsx
@@ -0,0 +1,19 @@
+import '../App.css'
+
+function HomeHead() {
+
+  return (
+      <>
+        <nav className='nav-menu'>
+          <ul className='nav-menu-items'>
+            <li>Home</li>
+            <li>About</li>
+            <li>Projects</li>
+            <li>Skills</li>
+          </ul>
+        </nav>
+      </>
+  )
+}
+
+export default HomeHead
