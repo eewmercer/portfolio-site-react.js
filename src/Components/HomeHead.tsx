832acc3099b8268022e287fb7815e17310781f0e
Emmaline Mercer <eewmercer@gmail.com>
2024-09-18 23:43:47 -0400

    Add home page header image

--- src/Components/HomeHead.tsx
+++ src/Components/HomeHead.tsx
@@ -1,15 +1,17 @@
-import '../App.css'
+import '../App.scss'
+import nameIntro from '../assets/nameIntro.svg'
 
 function HomeHead() {
 
   return (
       <>
         <nav className='nav-menu'>
+          <img src={nameIntro}/>
           <ul className='nav-menu-items'>
-            <li>Home</li>
-            <li>About</li>
-            <li>Projects</li>
-            <li>Skills</li>
+            <li><div>Home</div></li>
+            <li><div>About</div></li>
+            <li><div>Projects</div></li>
+            <li><div>Skills</div></li>
           </ul>
         </nav>
       </>
